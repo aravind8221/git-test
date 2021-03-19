6dcdd74c3e0e638751d2498836539dfad3d4937a
aravind8221 <80830700+aravind8221@users.noreply.github.com>
2021-03-19 15:50:43 +0530

    Create MenuItemDao.java

---
+++ truYum/src/com/cognizant/truyum/dao/MenuItemDao.java
@@ -0,0 +1,11 @@
+package com.cognizant.truyum.dao;
+import java.util.List;
+import com.cognizant.truyum.model.*;
+
+public interface MenuItemDao {
+	List<MenuItem> getMenuItemListAdmin();
+	List<MenuItem> getMenuItemListCustomer();
+	void modifyMenuItem(MenuItem menuItem);
+	MenuItem getMenuItem(long menuItemId); 
+
+}
